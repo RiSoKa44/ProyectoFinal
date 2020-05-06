7c39ad8e6e9cce9a89f9b3e575dae135e1d7f9e3
Javier Sußrez <a14javsuaarc@inspedralbes.cat>
2020-05-06 10:41:52 +0200

    Añadido muestra de Jugadores y Entrenadores
    Se pueden ver los jugadoes y entrenadores

--- js/funcions.js
+++ js/funcions.js
@@ -2,13 +2,18 @@ var Fantasy = new Vue({
     el: '#proyecto',
     data: {
         equipos: [],
-        juegos: null,
-        ligas: null,
+        juegos: [],
+        ligas: [],
+        jugadores: [],
+        entrenadores: [],
     },
     created: function() {
         this.GETTODOS();
         this.GETTODOS2();
         this.GETTODOS3();
+        this.GETTODOS4();
+        this.GETTODOS5();
+
     },
 
     methods: {
@@ -32,6 +37,22 @@ var Fantasy = new Vue({
             axios.get('http://esports-madness.electronica-garcilaso.cat/API/api.php/records/Ligas')
                 .then(response => {
                     this.ligas = response.data.records,
+                        console.table(response.data);
+                })
+                .catch(error => console.error(error));
+        },
+        GETTODOS4: function() {
+            axios.get('http://esports-madness.electronica-garcilaso.cat/API/api.php/records/Jugador')
+                .then(response => {
+                    this.jugadores = response.data.records,
+                        console.table(response.data);
+                })
+                .catch(error => console.error(error));
+        },
+        GETTODOS5: function() {
+            axios.get('http://esports-madness.electronica-garcilaso.cat/API/api.php/records/Entrenadores')
+                .then(response => {
+                    this.entrenadores = response.data.records,
                         console.table(response.data);
                 })
                 .catch(error => console.error(error));
