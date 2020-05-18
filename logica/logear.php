8d82b875586f1627df1aec637533cdbffd9e5394
Javier Sußrez <a14javsuaarc@inspedralbes.cat>
2020-05-18 10:58:00 +0200

    Logear.php actualizado

--- logica/logear.php
+++ logica/logear.php
@@ -2,17 +2,25 @@
 require 'conexion.php';
 session_start();
 
-$Nombre= $_POST['Nombre'];
-$Contraseña= $_POST['Contraseña'];
 
-$query = "SELECT * FROM Usuario WHERE Nombre='" . $Nombre. "' AND Contraseña='" .$Contraseña. "'";
-$result = mysqli_query($conexion, $query);
+$Nombre= $_POST['Nombre'];
+$Contrasena= $_POST['Contraseña'];
+
+$query = "SELECT * FROM Usuario WHERE Nombre='" . $Nombre. "' AND Contrasena='" .$Contrasena. "'";
+
+$result = mysqli_query($conn, $query);
+$numRows = mysqli_num_rows($result);
+
+print_r($numRows);
+
 $array= mysqli_fetch_array($result);
 
-if ($array['contar']>0){
+if ($numRows>0){
     $_SESSION['Nombre']=$Nombre;
     header("Location: ../PaginaPrincipal.php");
 }else{
+    echo $query;
+
     echo "datos incorrectos";
 }
 
